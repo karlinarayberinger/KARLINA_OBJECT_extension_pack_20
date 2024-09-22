e1c23364a52a587d33b04e938c5fce7e9d982a7f
karbytes <karlina.ray.beringer@protonmail.com>
2024-09-21 23:48:49 -0700

    Add files via upload

---
+++ milliseconds_since_unix_epoch.py
@@ -0,0 +1,13 @@
+import time
+
+# Get the current time in seconds since the Unix Epoch
+current_time_seconds = time.time()
+
+# Convert to milliseconds by multiplying by 1000 and casting to an integer
+milliseconds_since_epoch = int(current_time_seconds * 1000)
+
+# Open (or create if it doesn't exist) the file x.txt and write the milliseconds
+with open("x.txt", "w") as file:
+    file.write(str(milliseconds_since_epoch))
+
+print(f"Milliseconds since Unix Epoch: {milliseconds_since_epoch} has been written to x.txt.")
